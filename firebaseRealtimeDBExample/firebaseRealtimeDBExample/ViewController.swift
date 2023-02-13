f51ab2e21d83f8225c4b189cd6ed8ec737516605
dem <>
2023-02-14 00:38:33 +0300

    FirebaseDatabase implementation done

--- firebaseRealtimeDBExample/firebaseRealtimeDBExample/ViewController.swift
+++ firebaseRealtimeDBExample/firebaseRealtimeDBExample/ViewController.swift
@@ -5,15 +5,15 @@
 //  Created by Dilda Ezgi Metincan on 13.02.2023.
 //
 
+import FirebaseDatabase
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let database = Database.database().reference()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    } 
-
+    }
 
 }
 
